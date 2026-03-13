aab1262f0aabc2bd3bdf7af00222b24b8dd54d32
Swapnil Bharshankar <swapnilbharshankar@Swapnils-MacBook-Air.local>
2026-03-13 12:36:48 +0530

    fix(azure): network - RT - subnet

--- azure/network/main.tf
+++ azure/network/main.tf
@@ -10,15 +10,16 @@ module "avm-res-network-virtualnetwork" {
 }
 
 resource "azurerm_route_table" "public" {
-  location            = var.location
-  name                = "${var.name}-public-route-table"
-  resource_group_name = var.resource_group_name
+    location            = var.location
+    name                = "${var.name}-public-route-table"
+    resource_group_name = var.resource_group_name
+    subnets             = [for subnet in var.subnets : [for item in subnet: item.name if contains(item.name, "public")]]
 }
 
 resource "azurerm_route" "public" {
-  address_prefix      = "0.0.0/0"
-  name                = "${var.name}-public-route"
-  next_hop_type       = "Internet"
-  resource_group_name = var.resource_group_name
-  route_table_name    = azurerm_route_table.public.name
+    address_prefix      = "0.0.0/0"
+    name                = "${var.name}-public-route"
+    next_hop_type       = "Internet"
+    resource_group_name = var.resource_group_name
+    route_table_name    = azurerm_route_table.public.name
 }
