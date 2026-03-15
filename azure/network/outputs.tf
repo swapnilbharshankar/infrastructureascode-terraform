57048e7258ed111a33c8b665d70379e4faecbd53
Swapnil Bharshankar <swapnilbharshankar@Swapnils-MacBook-Air.local>
2026-03-15 10:17:36 +0530

    feature(azure): fix vnet output: subnet

--- azure/network/outputs.tf
+++ azure/network/outputs.tf
@@ -1,7 +1,7 @@
 output "subnet_ids" {
   description = "A map of subnet IDs created by the virtual network module."
   value = {
-    for name, subnet in module.your_vnet_module_name.subnets :
+    for name, subnet in module.avm-res-network-virtualnetwork.subnets :
     name => subnet.resource_id
   }
 }
